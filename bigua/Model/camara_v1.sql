c85da173f9dd3f757be01e4a8aea6081edc4f866
Alifer Sales <alifer10@gmail.com>
2018-11-02 19:55:23 -0300

    Inserindo obter_detalhes_deputado sql create table

--- bigua/Model/camara_v1.sql
+++ bigua/Model/camara_v1.sql
@@ -2,10 +2,10 @@ CREATE SCHEMA camara_v1;
 
 CREATE TABLE camara_v1.obter_deputados (
 	ide_cadastro            INT,
-    cod_orcamento           INT,
+	cod_orcamento           INT,
 	condicao                TEXT,
-    matricula               INT,
-    id_parlamentar          INT,
+	matricula               INT,
+	id_parlamentar          INT,
 	nome                    TEXT,
 	nome_parlamentar        TEXT,
 	url_foto                TEXT,
@@ -16,10 +16,35 @@ CREATE TABLE camara_v1.obter_deputados (
 	anexo                   TEXT,
 	fone                    TEXT,
 	email                   TEXT,
-    data_captura            TIMESTAMP,
-    url_captura             TEXT
+	data_captura            TIMESTAMP,
+	url_captura             TEXT
 );
 
-CREATE INDEX ide_cadastro_idx ON camara_v1.obter_deputados (ide_cadastro);
+CREATE INDEX ide_cadastro_idx_obter_deputados ON camara_v1.obter_deputados (ide_cadastro);
 
+
+CREATE TABLE camara_v1.obter_detalhes_deputado (
+	ide_cadastro			INT,
+	id_parlamentar_deprecated	INT,
+	num_legislatura			INT,
+	nome_parlamentar_atual		TEXT,
+	nome_civil			TEXT,
+	partido_atual_id_partido	TEXT,
+	partido_atual_sigla		TEXT,
+	partido_atual_nome		TEXT,
+	uf_representacao_atual		TEXT,
+	sexo				TEXT,
+	email				TEXT,
+	data_nascimento			DATE,
+	data_falecimento		DATE,
+	nome_profissao			TEXT,
+	gabinete_numero			INT,
+	ganinete_anexo			INT,
+	gabinete_telefone		TEXT,
+	situacao_na_legislatura_atual	TEXT,
+	data_captura            TIMESTAMP,
+	url_captura             TEXT
+);
+
+CREATE INDEX ide_cadastro_idx_obter_detalhes_deputados ON camara_v1.obter_detalhes_deputados (ide_cadastro);
 
